489a5e96d1d1897c763ecdb302bc518cebaf13aa
akoskaaa <akos.hochrein@prezi.com>
2017-12-25 10:30:58 +0100

    ignore prefixes now supported

--- README.md
+++ README.md
@@ -10,3 +10,19 @@ pip install -g 3debt
 ```
 3debt requirements.txt
 ```
+
+```
+$ 3debt -h
+usage: 3debt [-h] [--ignore [IGNORE [IGNORE ...]]] filename
+
+Get information if your dependent packages are Python 3 compatible yet
+
+positional arguments:
+  filename              Name of the file that contains the dependencies
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --ignore [IGNORE [IGNORE ...]]
+                        Prefixes of packages that you'd like to ignore while
+                        parsing the file
+```
--- src/__init__.py
+++ src/__init__.py
@@ -3,27 +3,34 @@ import requests
 
 
 parser = argparse.ArgumentParser(description="""
-    Get information if your packages are Python 3 compatible yet
+    Get information if your dependent packages are Python 3 compatible yet
 """.strip())
-
+parser.add_argument('--ignore',
+    help="Prefixes of packages that you\'d like to ignore while parsing the file",
+    nargs='*', default=[]
+)
 parser.add_argument('filename',
-    help='Name of the file that contains the dependencies'
+    help="Name of the file that contains the dependencies",
 )
 
 def run():
     args = parser.parse_args()
-    parse_file(args.filename)
+    parse_file(args.filename, args.ignore)
 
 
-def parse_file(filename):
+def parse_file(filename, ignored_prefixes):
     with open(filename, 'r') as f:
         for line in f.readlines():
-            parse_line(line.strip())
+            parse_line(line.strip(), ignored_prefixes)
 
 
-def parse_line(line):
+def parse_line(line, ignored_prefixes):
     if not len(line) or line.startswith('#'):
         return
+
+    if any([line.startswith(prefix) for prefix in ignored_prefixes]):
+        return
+
     package_name, package_version = line.split('==')
     print('{indicator}-{package_name}@{package_version}'.format(
         indicator='OK' if is_package_3_good(package_name, package_version) else 'NO',
